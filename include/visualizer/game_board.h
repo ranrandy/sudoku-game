0323fbbe76dcfcca05a9d4b6e24b4c03f7af23f8
ranrandy <runfeng4@illinois.edu>
2021-04-19 01:33:23 +0800

    Implement generating board on the screen

---
+++ include/visualizer/game_board.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Rand.h"
+#include "core/sudoku_board.h"
+
+namespace sudokugame {
+
+namespace visualizer {
+
+using sudokugame::SudokuBoard;
+using glm::vec2;
+
+class GameBoard {
+public:
+  enum class Level {
+    kEasy, 
+    kMedium, 
+    kHard
+  };
+  GameBoard(const vec2& board_top_left, const vec2& board_right_bottom, 
+            size_t board_size, Level level);
+
+  void Draw();
+
+private:
+  const ci::Color kBoardColor = "white";
+  const ci::Color kSquareEdgeColor = "black";
+
+  const size_t kDefaultNumberTotal = 30;
+  const size_t kEasyLevelMaxTotal = 44;
+  const size_t kEasyLevelMinTotal = 39;
+  const size_t kMediumLevelMaxTotal = 38;
+  const size_t kMediumLevelMinTotal = 33;
+  const size_t kHardLevelMaxTotal = 32;
+  const size_t kHardLevelMinTotal = 22;
+  
+  const float kSubBoardLineWidth = 3.0;
+  const float kTileLineWidth = 1.0;
+  
+  void DrawSquares(size_t square_length, size_t edge_line_width, size_t number);
+  void DrawNumbers();
+  
+  size_t board_size_;
+  
+  vec2 board_top_left_;
+  vec2 board_bottom_right_;
+  
+  SudokuBoard sudoku_board;
+};
+
+} // visualizer
+
+} // sudokugame
---
+++ src/visualizer/game_board.cpp
@@ -0,0 +1,92 @@
+#include "visualizer/game_board.h"
+
+namespace sudokugame {
+
+namespace visualizer {
+
+GameBoard::GameBoard(const vec2& board_top_left, 
+                     const vec2& board_bottom_right, 
+                     size_t board_size,
+                     Level level) 
+    : board_size_(board_size), 
+      board_top_left_(board_top_left),
+      board_bottom_right_(board_bottom_right), 
+      sudoku_board(SudokuBoard(board_size)) {
+  std::random_device rd;
+  switch (level) {
+  case Level::kEasy:
+    sudoku_board.GenerateValidBoard(rd() % (kEasyLevelMaxTotal - 
+                                            kEasyLevelMinTotal) + 
+                                    kEasyLevelMinTotal);
+    break;
+  case Level::kMedium:
+    sudoku_board.GenerateValidBoard(rd() % (kMediumLevelMaxTotal - 
+                                            kMediumLevelMinTotal) + 
+                                    kMediumLevelMinTotal);
+    break;
+  case Level::kHard:
+    sudoku_board.GenerateValidBoard(rd() % (kHardLevelMaxTotal -
+                                            kHardLevelMinTotal) + 
+                                    kHardLevelMinTotal);
+    break;
+  default:
+    sudoku_board.GenerateValidBoard(kDefaultNumberTotal);
+  }
+}
+
+void GameBoard::Draw() {
+  // Draw board contents
+  ci::Rectf board_bounding_box(board_top_left_, board_bottom_right_);
+  ci::gl::color(kBoardColor);
+  ci::gl::drawSolidRect(board_bounding_box);
+
+  // Draw Tiles
+  DrawSquares((board_bottom_right_.x - board_top_left_.x) / board_size_,
+              kTileLineWidth, board_size_ * board_size_);
+
+  // Draw sub boards
+  DrawSquares((board_bottom_right_.x - board_top_left_.x) / sqrt(board_size_),
+              kSubBoardLineWidth, board_size_);
+
+  // Draw original numbers
+  DrawNumbers();
+}
+
+void GameBoard::DrawSquares(size_t square_length, size_t edge_line_width, 
+                            size_t number) {
+  for (size_t row = 0; row < sqrt(number); ++row) {
+    for (size_t col = 0; col < sqrt(number); ++col) {
+      vec2 square_top_left = board_top_left_ + 
+                             vec2(row * square_length,
+                                  col * square_length);
+      vec2 square_bottom_right = board_top_left_ + 
+                                 vec2((row + 1) * square_length,
+                                      (col + 1) * square_length);
+      ci::Rectf square_bounding_box(square_top_left, 
+                                    square_bottom_right);
+      ci::gl::color(kSquareEdgeColor);
+      ci::gl::drawStrokedRect(square_bounding_box, edge_line_width);
+    }
+  }
+}
+
+void GameBoard::DrawNumbers() {
+  size_t square_length = (board_bottom_right_.x - board_top_left_.x) / 
+                         board_size_;
+  for (size_t row = 0; row < sudoku_board.GetBoardSize(); ++row) {
+    for (size_t col = 0; col < sudoku_board.GetBoardSize(); ++col) {
+      if (sudoku_board.GetBoardNumbers()[row][col] != 0) {
+        vec2 square_center = board_top_left_ +
+                             vec2((row + 0.5) * square_length,
+                                  (col + 0.5) * square_length);
+        ci::gl::drawStringCentered(
+            std::to_string(sudoku_board.GetBoardNumbers()[row][col]),
+            square_center, kSquareEdgeColor);
+      }
+    }
+  }
+}
+
+} // visualizer
+
+} // sudokugame
